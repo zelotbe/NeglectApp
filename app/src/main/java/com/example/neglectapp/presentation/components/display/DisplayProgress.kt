13a141728e8892e99b99319f7689de6b46f41f63
Quintin <zelotbe@gmail.com>
2022-11-21 09:46:40 +0100

    ProgressIndicator

--- app/src/main/java/com/example/neglectapp/presentation/components/display/DisplayProgress.kt
+++ app/src/main/java/com/example/neglectapp/presentation/components/display/DisplayProgress.kt
@@ -3,9 +3,12 @@ package com.example.neglectapp.presentation.components.display
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.CircularProgressIndicator
 
 @Composable
 fun DisplayProgress(){
-//        CircularProgressIndicator(modifier = Modifier.fillMaxSize())
+    CircularProgressIndicator( progress = 0.4f,
+        modifier = Modifier.fillMaxSize(),
+        strokeWidth = 5.dp)
 }
