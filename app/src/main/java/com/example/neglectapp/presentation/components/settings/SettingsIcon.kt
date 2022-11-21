7da1594ea01f9e312d9521b027fb8e05f34c0ece
Quintin <zelotbe@gmail.com>
2022-11-21 14:41:22 +0100

    Fixed navigation

--- app/src/main/java/com/example/neglectapp/presentation/components/settings/SettingsIcon.kt
+++ app/src/main/java/com/example/neglectapp/presentation/components/settings/SettingsIcon.kt
@@ -11,19 +11,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Icon
+import com.example.neglectapp.presentation.navigation.Screen
 
 @Composable
 fun SettingsIcon(
-    onClick: () -> Unit
+    navController: NavHostController
 ) {
     Spacer(modifier = Modifier.height(15.dp))
     Box(
         modifier = Modifier,
     ){
         Button(
-            onClick = { /*TODO*/ },
+            onClick = { navController.navigate(Screen.Settings.route) },
             modifier = Modifier.size(25.dp).background(Color.Black.copy(alpha = 0f))
         ){
             Icon(
