b0702af2ade13780dec6c4fd838046929cdf3fe7
Quintin <zelotbe@gmail.com>
2022-11-22 12:32:23 +0100

    3 Stimula
    Still WIP

--- app/src/main/java/com/example/neglectapp/presentation/ui/settings/stimulans/StimulaScreen.kt
+++ app/src/main/java/com/example/neglectapp/presentation/ui/settings/stimulans/StimulaScreen.kt
@@ -1,31 +1,80 @@
 package com.example.neglectapp.presentation.ui.settings.stimulans
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.wear.compose.material.Chip
-import androidx.wear.compose.material.Icon
-import androidx.wear.compose.material.ScalingLazyColumn
-import androidx.wear.compose.material.Text
-import com.example.neglectapp.R
-import com.example.neglectapp.presentation.navigation.Screen
+import androidx.wear.compose.material.*
 
 @Composable
 fun DisplayStimula(
     status: Boolean = false,
     modifier: Modifier,
+    navController: NavHostController
 ){
+    var checked by remember { mutableStateOf(true) }
     Column(
         modifier = Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-
-
+        ScalingLazyColumn(
+            horizontalAlignment = Alignment.Start,
+        ){
+            item(){
+                SplitToggleChip(
+                    checked = checked,
+                    onCheckedChange = {},
+                    onClick = {  },
+                    toggleControl = {
+                        Switch(
+                            checked = checked,
+                            onCheckedChange = { checked = it }
+                        )
+                    },
+                    label = {
+                        Text("Vibratie")
+                    },
+                    enabled = false,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            item() {
+                SplitToggleChip(
+                    checked = checked,
+                    onCheckedChange = {},
+                    onClick = {  },
+                    toggleControl = {
+                        Switch(
+                            checked = checked,
+                            onCheckedChange = { checked = it }
+                        )
+                    },
+                    label = {
+                        Text("Geluid")
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            item(){
+                SplitToggleChip(
+                    checked = checked,
+                    onCheckedChange = {},
+                    onClick = {  },
+                    toggleControl = {
+                        Switch(
+                            checked = checked,
+                            onCheckedChange = { checked = it }
+                        )
+                    },
+                    label = {
+                        Text("Licht")
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
     }
 }
