b8802d6efda771b5503749f899cdf2048fbc64a5
Quintin <zelotbe@gmail.com>
2022-11-22 11:34:21 +0100

    Start of stimula screen

---
+++ app/src/main/java/com/example/neglectapp/presentation/ui/settings/stimulans/StimulaScreen.kt
@@ -0,0 +1,31 @@
+package com.example.neglectapp.presentation.ui.settings.stimulans
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.ScalingLazyColumn
+import androidx.wear.compose.material.Text
+import com.example.neglectapp.R
+import com.example.neglectapp.presentation.navigation.Screen
+
+@Composable
+fun DisplayStimula(
+    status: Boolean = false,
+    modifier: Modifier,
+){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+
+    }
+}
