d2ca591024755547ed2453b8f772e747d643e299
Quintin <zelotbe@gmail.com>
2022-12-07 21:30:50 +0100

    OperatingViewModel
    Used for switching start & end hours in OperatingHoursScreen

---
+++ app/src/main/java/com/example/neglectapp/ui/operatinghours/OperatingViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.neglectapp.ui.operatinghours
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class OperatingViewModel: ViewModel() {
+
+    private val _showEnd = MutableStateFlow(false)
+
+    val showEnd: StateFlow<Boolean> = _showEnd
+
+    fun toggleEnd(){
+        _showEnd.value.let {
+            _showEnd.value = !it
+        }
+    }
+}
