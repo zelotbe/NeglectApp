3797cbf4021f68556d945d5d5ebbf1afd5799f5b
Quintin <zelotbe@gmail.com>
2022-12-12 22:39:41 +0100

    WIP

--- app/src/main/java/com/example/neglectapp/ui/status/StatusScreen.kt
+++ app/src/main/java/com/example/neglectapp/ui/status/StatusScreen.kt
@@ -1,18 +1,26 @@
 package com.example.neglectapp.ui.status
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.wear.compose.material.Text
 import com.example.neglectapp.components.display.DisplayProgress
+import com.example.neglectapp.data.datastore.StoreSessions
+import com.example.neglectapp.util.SessionState
+import java.time.LocalTime
 
 @Composable
 fun DisplayStatus(
-    status: Boolean = false,
+    status: SessionState,
     modifier: Modifier,
+    sessionStore: StoreSessions
+
 ){
-    if (status){
-        Text("08:30 - 16:00")
-        DisplayProgress()
+    val startHour = sessionStore.getStart.collectAsState(initial = LocalTime.of(7,30) )
+    val endHour = sessionStore.getEnd.collectAsState(initial = LocalTime.of(16,0))
+
+    if (status == SessionState.Started || status == SessionState.Stopped){
+        Text("${startHour.value} - ${endHour.value}")
     }else{
         Text("Geen actieve sessie")
     }
