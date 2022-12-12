1de92227d020088c93738be5b35f3a9abc68cd56
Quintin <zelotbe@gmail.com>
2022-12-12 22:34:32 +0100

    Create Constants.kt

---
+++ app/src/main/java/com/example/neglectapp/util/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.neglectapp.util
+
+object Constants {
+        const val ACTION_SERVICE_START = "ACTION_SERVICE_START"
+        const val ACTION_SERVICE_STOP = "ACTION_SERVICE_STOP"
+        const val ACTION_SERVICE_CANCEL = "ACTION_SERVICE_CANCEL"
+
+        const val SESSION_STATE = "SESSION_STATE"
+
+        const val NOTIFICATION_CHANNEL_ID = "SESSION_NOTIFICATION_ID"
+        const val NOTIFICATION_CHANNEL_NAME = "SESSION_NOTIFICATION"
+        const val NOTIFICATION_ID = 10
+
+        const val CLICK_REQUEST_CODE = 100
+        const val CANCEL_REQUEST_CODE = 101
+        const val STOP_REQUEST_CODE = 102
+        const val RESUME_REQUEST_CODE = 103
+}
