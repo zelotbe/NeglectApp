9975276fc4f288c9d9b2b1142c19d6aca1f73d8d
Quintin <zelotbe@gmail.com>
2022-12-12 22:38:56 +0100

    WIP

--- app/src/main/java/com/example/neglectapp/components/display/DisplayProgress.kt
+++ app/src/main/java/com/example/neglectapp/components/display/DisplayProgress.kt
@@ -2,12 +2,21 @@ package com.example.neglectapp.components.display
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.CircularProgressIndicator
+import com.example.neglectapp.data.datastore.StoreSessions
+import com.example.neglectapp.util.calculateSeconds
+import java.time.LocalTime
 
 @Composable
-fun DisplayProgress(){
+fun DisplayProgress(sessionStore: StoreSessions){
+    val startHour = sessionStore.getStart.collectAsState(initial = LocalTime.of(7,30).toString()).value
+    val endHour = sessionStore.getEnd.collectAsState(initial = LocalTime.of(16,0).toString()).value
+    val startSeconds = calculateSeconds(LocalTime.parse(startHour))
+    val endSeconds = calculateSeconds(LocalTime.parse(endHour))
+    val diffHours = endSeconds - startSeconds
     CircularProgressIndicator( progress = 0.4f,
         modifier = Modifier.fillMaxSize(),
         strokeWidth = 5.dp)
