dde1538a0860286f4cbf6fd6c91491ef2d8ecec5
Quintin <zelotbe@gmail.com>
2022-12-12 22:32:14 +0100

    Session Foregroundservice WIP

---
+++ app/src/main/java/com/example/neglectapp/util/SessionService.kt
@@ -0,0 +1,221 @@
+package com.example.neglectapp.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.Build
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.mutableStateOf
+import androidx.core.app.NotificationCompat
+import androidx.wear.ongoing.Status
+import com.example.neglectapp.util.Constants.ACTION_SERVICE_CANCEL
+import com.example.neglectapp.util.Constants.ACTION_SERVICE_START
+import com.example.neglectapp.util.Constants.ACTION_SERVICE_STOP
+import com.example.neglectapp.util.Constants.NOTIFICATION_CHANNEL_ID
+import com.example.neglectapp.util.Constants.NOTIFICATION_CHANNEL_NAME
+import com.example.neglectapp.util.Constants.NOTIFICATION_ID
+import com.example.neglectapp.util.Constants.SESSION_STATE
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import javax.inject.Inject
+import kotlin.concurrent.fixedRateTimer
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+@ExperimentalAnimationApi
+@AndroidEntryPoint
+class SessionService() : Service() {
+    @Inject
+    lateinit var notificationManager: NotificationManager
+    @Inject
+    lateinit var notificationBuilder: NotificationCompat.Builder
+
+    private val binder = SessionBinder()
+
+    private var duration: Duration = Duration.ZERO
+    private lateinit var timer: Timer
+
+    var seconds = mutableStateOf("00")
+        private set
+    var minutes = mutableStateOf("00")
+        private set
+    var hours = mutableStateOf("00")
+        private set
+
+    var currentState = mutableStateOf(SessionState.Idle)
+        private set
+    
+    override fun onBind(p0: Intent?) = binder
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.getStringExtra(SESSION_STATE)) {
+            SessionState.Started.name -> {
+                setStopButton()
+                startForegroundService()
+                startStopwatch { hours, minutes, seconds ->
+                    updateNotification(hours = hours, minutes = minutes, seconds = seconds)
+                }
+            }
+            SessionState.Stopped.name -> {
+                stopStopwatch()
+                setResumeButton()
+            }
+            SessionState.Canceled.name -> {
+                stopStopwatch()
+                cancelStopwatch()
+                stopForegroundService()
+            }
+        }
+        intent?.action.let {
+            when (it) {
+                ACTION_SERVICE_START -> {
+                    setStopButton()
+                    startForegroundService()
+                    startStopwatch { hours, minutes, seconds ->
+                        updateNotification(hours = hours, minutes = minutes, seconds = seconds)
+                    }
+                }
+                ACTION_SERVICE_STOP -> {
+                    stopStopwatch()
+                    setResumeButton()
+                }
+                ACTION_SERVICE_CANCEL -> {
+                    stopStopwatch()
+                    cancelStopwatch()
+                    stopForegroundService()
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+    private fun startStopwatch(onTick: (h: String, m: String, s: String) -> Unit) {
+        currentState.value = SessionState.Started
+        timer = fixedRateTimer(initialDelay = 1000L, period = 1000L) {
+            duration = duration.plus(1.seconds)
+            updateTimeUnits()
+            onTick(hours.value, minutes.value, seconds.value)
+        }
+    }
+
+    private fun stopStopwatch() {
+        if (this::timer.isInitialized) {
+            timer.cancel()
+        }
+        currentState.value = SessionState.Stopped
+    }
+
+    private fun cancelStopwatch() {
+        duration = Duration.ZERO
+        currentState.value = SessionState.Idle
+        updateTimeUnits()
+    }
+
+    private fun updateTimeUnits() {
+        duration.toComponents { hours, minutes, seconds, _ ->
+            this@SessionService.hours.value = hours.toInt().pad()
+            this@SessionService.minutes.value = minutes.pad()
+            this@SessionService.seconds.value = seconds.pad()
+        }
+    }
+
+    private fun startForegroundService() {
+        createNotificationChannel()
+        val ongoingActivityStatus = Status.Builder()
+            // Sets the text used across various surfaces.
+            .addTemplate("ONGOING ACTIVITY")
+            .build()
+
+//        val ongoingActivity =
+//            OngoingActivity.Builder(applicationContext, NOTIFICATION_ID, notificationBuilder)
+//                // Sets icon that will appear on the watch face in active mode. If it isn't set,
+//                // the watch face will use the static icon in active mode.
+////                .setAnimatedIcon(R.drawable.animated_walk)
+//                // Sets the icon that will appear on the watch face in ambient mode.
+//                // Falls back to Notification's smallIcon if not set. If neither is set,
+//                // an Exception is thrown.
+//                .setStaticIcon(R.drawable.ic_baseline_flaky_24)
+//                // Sets the tap/touch event, so users can re-enter your app from the
+//                // other surfaces.
+//                // Falls back to Notification's contentIntent if not set. If neither is set,
+//                // an Exception is thrown.
+//                .setTouchIntent(ServiceHelper.clickPendingIntent(context))
+//                // In our case, sets the text used for the Ongoing Activity (more options are
+//                // available for timers and stop watches).
+//                .setStatus(ongoingActivityStatus)
+//                .build()
+//
+//        // Applies any Ongoing Activity updates to the notification builder.
+//        // This method should always be called right before you build your notification,
+//        // since an Ongoing Activity doesn't hold references to the context.
+//        ongoingActivity.apply(applicationContext)
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun stopForegroundService() {
+        notificationManager.cancel(NOTIFICATION_ID)
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_LOW
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun updateNotification(hours: String, minutes: String, seconds: String) {
+        notificationManager.notify(
+            NOTIFICATION_ID,
+            notificationBuilder.setContentText(
+                "Hello"
+//                formatTime(
+//                    hours = hours,
+//                    minutes = minutes,
+//                    seconds = seconds,
+//                )
+            ).build()
+        )
+    }
+
+    private fun setStopButton() {
+        notificationBuilder.mActions.removeAt(0)
+        notificationBuilder.mActions.add(
+            0,
+            NotificationCompat.Action(
+                0,
+                "Stop",
+                ServiceHelper.stopPendingIntent(this)
+            )
+        )
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun setResumeButton() {
+        notificationBuilder.mActions.removeAt(0)
+        notificationBuilder.mActions.add(
+            0,
+            NotificationCompat.Action(
+                0,
+                "Resume",
+                ServiceHelper.resumePendingIntent(this)
+            )
+        )
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+    inner class SessionBinder : Binder() {
+        fun getService(): SessionService = this@SessionService
+    }
+}
+enum class SessionState {
+    Idle,
+    Started,
+    Stopped,
+    Canceled
+}
