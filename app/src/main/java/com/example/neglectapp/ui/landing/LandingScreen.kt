2033ff0ef66b80a3359f315ec4be1d8800524897
Quintin <zelotbe@gmail.com>
2022-12-13 17:51:41 +0100

    Added an option to stop the session

--- app/src/main/java/com/example/neglectapp/ui/landing/LandingScreen.kt
+++ app/src/main/java/com/example/neglectapp/ui/landing/LandingScreen.kt
@@ -1,29 +1,32 @@
 package com.example.neglectapp.ui.landing
 
 import NeglectButton
+import android.se.omapi.Session
 import android.util.Log
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.material.icons.filled.Stop
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.MaterialTheme
 import com.example.neglectapp.components.display.DisplayProgress
 import com.example.neglectapp.components.settings.SettingsIcon
 import com.example.neglectapp.data.datastore.StoreSessions
 import com.example.neglectapp.ui.status.DisplayStatus
 import com.example.neglectapp.util.*
+import com.example.neglectapp.util.Constants.ACTION_SERVICE_CANCEL
 import com.example.neglectapp.util.Constants.ACTION_SERVICE_START
 import com.example.neglectapp.util.Constants.ACTION_SERVICE_STOP
 import java.time.LocalTime
@@ -40,6 +43,9 @@ fun DisplayLanding(
     val sessionStore = StoreSessions(context)
     val startHour = sessionStore.getStart.collectAsState(initial = LocalTime.of(7,30).toString() )
     val endHour = sessionStore.getEnd.collectAsState(initial = LocalTime.of(16,0).toString())
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val color = if (currentState !== SessionState.Canceled) Color.Blue else Color.Gray
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -59,19 +65,32 @@ fun DisplayLanding(
                 SettingsIcon(navController = navController)
                 Spacer(modifier = Modifier.height(60.dp))
                 DisplayStatus(modifier = Modifier, status = currentState, sessionStore = sessionStore)
-                Spacer(modifier = Modifier.height(30.dp))
-                Column(modifier = Modifier.size(45.dp)) {
+                Spacer(modifier = Modifier.height(15.dp))
+                Row(modifier = Modifier) {
                     NeglectButton(
                         modifier = Modifier,
                         contentDescription = if(currentState == SessionState.Idle || currentState == SessionState.Stopped)  "Starten" else "Pauzeren",
                         icon = if(currentState == SessionState.Idle || currentState == SessionState.Stopped)  Icons.Default.PlayArrow else Icons.Default.Pause,
                         onClick = {
-                            Log.d("LANDING:", "CLICKED")
                             ServiceHelper.triggerForegroundService(
                                 context = context,
                                 action = if (currentState == SessionState.Started) ACTION_SERVICE_STOP
                                 else ACTION_SERVICE_START
-                            );Log.d("LANDING:", "SESSION STARTED/PAUSED")
+                            )
+                        }
+                    )
+                    Spacer(modifier = Modifier.width(25.dp))
+                    NeglectButton(
+                        modifier = Modifier,
+                        enabled = currentState!== SessionState.Idle,
+                        interactionSource = interactionSource,
+                        contentDescription = "Stoppen",
+                        icon = Icons.Default.Stop,
+                        onClick = {
+                            Log.d("StopButton:", "clicked");ServiceHelper.triggerForegroundService(
+                                context = context,
+                                action = ACTION_SERVICE_CANCEL
+                            )
                         }
                     )
                 }
