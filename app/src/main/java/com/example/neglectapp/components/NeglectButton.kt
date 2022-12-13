75148f0f08eff73138c87db3e9d4093b4f298969
Quintin <zelotbe@gmail.com>
2022-12-13 12:32:09 +0100

    Changed NeglectButton to only accept icons
    Icons are more UI/UX friendly on wearables

--- app/src/main/java/com/example/neglectapp/components/NeglectButton.kt
+++ app/src/main/java/com/example/neglectapp/components/NeglectButton.kt
@@ -1,13 +1,15 @@
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.*
 import com.example.neglectapp.util.ButtonType
@@ -15,29 +17,25 @@ import com.example.neglectapp.util.ButtonType
 
 @Composable
 fun NeglectButton(
-    type: ButtonType,
     modifier: Modifier,
-    label: String = "",
+    contentDescription: String = "Accept",
+    icon: ImageVector = Icons.Default.Check,
     color: androidx.compose.ui.graphics.Color = MaterialTheme.colors.primary,
     onClick: () -> Unit
 ){
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-    ) {
-        when (type) {
-            ButtonType.TEXT -> {
-                CompactChip(onClick = { onClick() }, label = { Text(label) }, modifier = Modifier.width(500.dp))
-            }
-            ButtonType.ICON -> {
-                Button(onClick = { onClick() }) {
+
+//        when (type) {
+//            ButtonType.TEXT -> {
+//                CompactChip(onClick = { onClick() }, label = { Text(label) }, modifier = Modifier.width(200.dp))
+//            }
+//            ButtonType.ICON -> {
+                Button(onClick = { onClick() }, modifier = Modifier.fillMaxSize()) {
                     Icon(
-                        Icons.Default.Check,
+                        icon,
                         contentDescription = "Accept",
                         modifier = Modifier.size(width = 35.dp, height = 35.dp)
                     )
                 }
-            }
-        }
+//            }
+//        }
     }
-}
