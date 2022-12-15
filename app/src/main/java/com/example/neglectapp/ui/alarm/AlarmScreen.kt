882f1ee2d37145f74c74e7422ea132e80e068b76
Quintin <zelotbe@gmail.com>
2022-12-15 22:45:28 +0100

    Removed vibrator & sound from here

--- app/src/main/java/com/example/neglectapp/ui/alarm/AlarmScreen.kt
+++ app/src/main/java/com/example/neglectapp/ui/alarm/AlarmScreen.kt
@@ -1,5 +1,6 @@
 package com.example.neglectapp.ui.alarm
 
+import android.app.Activity
 import android.content.Context
 import android.media.RingtoneManager
 import android.net.Uri
@@ -7,50 +8,67 @@ import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.util.Log
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.NavHostController
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
+import com.example.neglectapp.data.datastore.StoreSessions
+import com.example.neglectapp.data.datastore.StoreStimula
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun DisplayAlarm(
     modifier: Modifier = Modifier,
 ){
+    var context = LocalContext.current
+    val activity = (LocalContext.current as? Activity)
+    val stimulaStore = StoreStimula(context)
+    val getLight by stimulaStore.getLight.collectAsState(initial = false)
+    val sessionStore = StoreSessions(context)
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Black,
+        targetValue = Color.White,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colors.background),
+            .background(if(getLight == true){color} else {
+                MaterialTheme.colors.background
+            }
+            ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-        ) {
-            Text("ALARMmmmmmmmmmmmmmmmmmmmmmmmm")
-            var alarmUri: Uri? = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-            if (alarmUri == null) {
-                alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-            }
-            val r = RingtoneManager.getRingtone(LocalContext.current, alarmUri)
-            r.play()
-            val vibrator = LocalContext.current.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-            val vibrationEffect1: VibrationEffect =
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    VibrationEffect.createOneShot(100000, VibrationEffect.DEFAULT_AMPLITUDE)
-                } else {
-                    Log.e("TAG", "Cannot vibrate device..")
-                    TODO("VERSION.SDK_INT < O")
-                }
-
-            // it is safe to cancel other
-            // vibrations currently taking place
-            vibrator.cancel()
-            vibrator.vibrate(vibrationEffect1)
+        Spacer(modifier = Modifier.height(10.dp))
+        Text("Heftos Alarm")
+        Spacer(modifier = Modifier.height(50.dp))
+        Text("TIJD RESTEREND")
+        Spacer(modifier = Modifier.height(50.dp))
+        Button(onClick = { activity?.finish()}, modifier = Modifier) {
+            Icon(
+                Icons.Default.Check,
+                contentDescription = "Alarm accepteren",
+                modifier = Modifier.size(width = 35.dp, height = 35.dp)
+            )
         }
     }
 }
