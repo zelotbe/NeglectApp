62e13f0fc74e1d532180edd9ef73c353f890b9ca
Quintin <zelotbe@gmail.com>
2022-12-17 16:19:04 +0100

    Removed unused imports

--- app/src/main/java/com/example/neglectapp/AlarmActivity.kt
+++ app/src/main/java/com/example/neglectapp/AlarmActivity.kt
@@ -11,27 +11,12 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import androidx.wear.compose.material.Button
-import androidx.wear.compose.material.Icon
-import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.example.neglectapp.data.datastore.StoreSessions
 import com.example.neglectapp.data.datastore.StoreStimula
 import com.example.neglectapp.ui.alarm.DisplayAlarm
-import com.example.neglectapp.ui.theme.wearColorPalette
 
 class AlarmActivity : ComponentActivity() {
     @OptIn(ExperimentalAnimationApi::class)
@@ -46,9 +31,11 @@ class AlarmActivity : ComponentActivity() {
             val getSound by stimulaStore.getSound.collectAsState(initial = false)
             val sessionStore = StoreSessions(context)
             var alarmUri: Uri? = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+
             if (alarmUri == null) {
                 alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
             }
+
             r = RingtoneManager.getRingtone(context, alarmUri)
             vibrator =
                 context.getSystemService(VIBRATOR_SERVICE) as Vibrator
