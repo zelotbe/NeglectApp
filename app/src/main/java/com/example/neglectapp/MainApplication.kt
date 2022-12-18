328ac99dbe047e75f670d280e754a7eb324d94a4
Quintin <zelotbe@gmail.com>
2022-12-18 22:24:20 +0100

    Koin Implementation

--- app/src/main/java/com/example/neglectapp/MainApplication.kt
+++ app/src/main/java/com/example/neglectapp/MainApplication.kt
@@ -1,6 +1,32 @@
 package com.example.neglectapp
 import android.app.Application
+import android.content.Context
+import com.example.neglectapp.di.dependenciesAppModule
 import dagger.hilt.android.HiltAndroidApp
+import org.koin.core.component.KoinComponent
+import org.koin.core.context.startKoin
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application(), KoinComponent{
+    companion object {
+        private var instance: MainApplication? = null
+
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            // use Koin logger
+            printLogger()
+            // declare modules
+            modules(dependenciesAppModule)
+        }
+    }
+}
