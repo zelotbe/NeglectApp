ff5712d1ae6418759545d2a3269d450546fd28cb
Quintin <zelotbe@gmail.com>
2022-12-26 17:33:47 +0100

    Toast confirmation when alarm is accepted

--- app/src/main/java/com/example/neglectapp/ui/alarm/AlarmScreen.kt
+++ app/src/main/java/com/example/neglectapp/ui/alarm/AlarmScreen.kt
@@ -1,13 +1,7 @@
 package com.example.neglectapp.ui.alarm
 
 import android.app.Activity
-import android.content.Context
-import android.media.RingtoneManager
-import android.net.Uri
-import android.os.Build
-import android.os.VibrationEffect
-import android.os.Vibrator
-import android.util.Log
+import android.widget.Toast
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
@@ -21,19 +15,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
+import com.example.neglectapp.domain.model.HeftosSession
 import com.example.neglectapp.viewmodel.HeftosViewModel
+import com.example.neglectapp.viewmodel.SessionViewModel
 import kotlinx.coroutines.delay
+import java.time.LocalDateTime
 import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun DisplayAlarm(
     modifier: Modifier = Modifier,
+    sessionViewModel: SessionViewModel = hiltViewModel()
 ){
     var context = LocalContext.current
     val activity = (LocalContext.current as? Activity)
@@ -47,6 +46,7 @@ fun DisplayAlarm(
             repeatMode = RepeatMode.Reverse
         )
     )
+
     var secondsToDisappear by remember { mutableStateOf(15) }
 
     LaunchedEffect(Unit) {
@@ -54,6 +54,7 @@ fun DisplayAlarm(
             delay(1.seconds)
             secondsToDisappear -= 1
         }
+        sessionViewModel.addSession(HeftosSession(0, LocalDateTime.now(), false, 0))
         activity?.finish()
     }
     Column(
@@ -71,7 +72,7 @@ fun DisplayAlarm(
 
         Text("Tijd resterend: $secondsToDisappear")
         Spacer(modifier = Modifier.height(50.dp))
-        Button(onClick = { activity?.finish()}, modifier = Modifier) {
+        Button(onClick = { Toast.makeText(context, "Sessie opgeslagen", Toast.LENGTH_LONG).show();sessionViewModel.addSession(HeftosSession(0,LocalDateTime.now(), true, 0)) ;activity?.finish()}, modifier = Modifier) {
             Icon(
                 Icons.Default.Check,
                 contentDescription = "Alarm accepteren",
