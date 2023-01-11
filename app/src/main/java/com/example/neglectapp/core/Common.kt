12aa7b3ccbb5d730591a6f8debb5a97bc7ea32bd
Quintin <zelotbe@gmail.com>
2023-01-11 18:02:46 +0100

    Removed unused function

--- app/src/main/java/com/example/neglectapp/core/Common.kt
+++ app/src/main/java/com/example/neglectapp/core/Common.kt
@@ -1,6 +1,8 @@
 package com.example.neglectapp.core
 
+import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.ZoneId
 
 fun formatTime(seconds: String, minutes: String, hours: String): String {
     return "$hours:$minutes:$seconds"
@@ -8,17 +10,4 @@ fun formatTime(seconds: String, minutes: String, hours: String): String {
 
 fun Int.pad(): String {
     return this.toString().padStart(2, '0')
-}
-
-fun calculateSeconds(time: LocalTime): Int {
-    val timeHour = time.hour
-    val timeMinutes = time.minute
-    val timeSeconds = time.second
-    var hour = 0
-    var minutes = 0
-    var seconds = 0
-    hour = timeHour * 3600
-    minutes = timeMinutes * 60
-    seconds = timeSeconds
-    return hour + minutes + seconds
 }
