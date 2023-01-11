3dbc3086e6b0a9ddf3086e3c2c6f765c3abd0369
Quintin <zelotbe@gmail.com>
2023-01-11 19:39:20 +0100

    Moved calculateprogress

--- app/src/main/java/com/example/neglectapp/components/display/DisplayProgress.kt
+++ app/src/main/java/com/example/neglectapp/components/display/DisplayProgress.kt
@@ -1,17 +1,14 @@
 package com.example.neglectapp.components.display
 
-import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.material.CircularProgressIndicator
+import com.example.neglectapp.core.calculateProgress
 import com.example.neglectapp.viewmodel.HeftosViewModel
-import java.time.Duration
-import java.time.LocalTime
 
 @Composable
 fun DisplayProgress(
@@ -19,23 +16,8 @@ fun DisplayProgress(
 ){
     val startHour = viewModel.startHour.collectAsState(initial = "09:30").value
     val endHour = viewModel.endHour.collectAsState(initial = "19:00").value
-    @Composable
-    fun calculateProgress(): Float {
-        val start = LocalTime.parse(startHour)
-        val end = LocalTime.parse(endHour)
-        val currentTime = LocalTime.now()
 
-        val totalDuration = Duration.between(start, end).toMillis()
-        val elapsedDuration = Duration.between(start, currentTime).toMillis()
-
-        val percentage = (elapsedDuration.toFloat() / totalDuration.toFloat())
-
-        Log.d("Percentage:", "$percentage")
-
-        return percentage
-
-    }
-    CircularProgressIndicator( progress = calculateProgress(),
+    CircularProgressIndicator( progress = calculateProgress(startHour, endHour),
         modifier = Modifier.fillMaxSize(),
         strokeWidth = 5.dp)
 }
