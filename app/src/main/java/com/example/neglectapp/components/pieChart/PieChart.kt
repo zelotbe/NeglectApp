c779e9de52726877b15aae534bf5d6fd59405796
Quintin <zelotbe@gmail.com>
2023-01-12 22:38:37 +0100

    Changed text to Icons for UX

--- app/src/main/java/com/example/neglectapp/components/pieChart/PieChart.kt
+++ app/src/main/java/com/example/neglectapp/components/pieChart/PieChart.kt
@@ -2,10 +2,15 @@ package com.example.neglectapp.components.pieChart
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.MaterialTheme
@@ -14,17 +19,17 @@ import androidx.wear.compose.material.MaterialTheme
 fun PieChart(
     values: List<Int> = emptyList(),
     colors: List<Color> = listOf(MaterialTheme.colors.primaryVariant, MaterialTheme.colors.secondary),
-    legend: List<String> = listOf("Wel", "Niet"),
-    size: Dp = 150.dp
+    legend: List<ImageVector> = listOf( Icons.Default.Check, Icons.Default.Close),
+    size: Dp = 125.dp
 ) {
     val total = values.sum()
 
     if(total != 0) {
-        val proportions: List<Double> = values.map {
+        val percentages: List<Double> = values.map {
             it * 100.0 / total
         }
 
-        val sweepAngles = proportions.map {
+        val sweepAngles = percentages.map {
             360 * it / 100
         }
 
@@ -50,8 +55,8 @@ fun PieChart(
 
         Column {
             for (i in values.indices) {
-                DisplayLegend(color = colors[i], legend = legend[i])
-                Spacer(modifier = Modifier.width(5.dp))
+                DisplayLegend(color = colors[i], legend = legend[i], percentage = percentages[i])
+                Spacer(modifier = Modifier.width(8.dp))
             }
         }
     }
