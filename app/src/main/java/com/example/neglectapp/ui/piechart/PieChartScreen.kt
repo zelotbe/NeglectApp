32e1831c29182ba6aed8055aa3b54ff267d6398b
Quintin <zelotbe@gmail.com>
2023-01-12 22:37:01 +0100

    Made screen scrollable + subtitle for clearity

--- app/src/main/java/com/example/neglectapp/ui/piechart/PieChartScreen.kt
+++ app/src/main/java/com/example/neglectapp/ui/piechart/PieChartScreen.kt
@@ -1,6 +1,10 @@
 package com.example.neglectapp.ui.piechart
 
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -16,18 +20,22 @@ import com.example.neglectapp.viewmodel.SessionViewModel
 fun DisplayPieChart(
     navController: NavHostController,
     modifier: Modifier,
+    scrollState: ScrollState,
     viewModel: SessionViewModel = hiltViewModel()
 ){
     val amountInteracted = viewModel.amountInteracted.collectAsState(initial = 0)
     val amountNotInteracted = viewModel.amountNotInteracted.collectAsState(initial = 0)
     Column(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .scrollable(scrollState, Orientation.Vertical)
+            .verticalScroll(scrollState),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Spacer(modifier = Modifier.height(10.dp))
         Text("Data")
+        Text("Interactie")
         Spacer(modifier = Modifier.height(10.dp))
         PieChart(values = listOf(amountInteracted.value, amountNotInteracted.value))
     }
