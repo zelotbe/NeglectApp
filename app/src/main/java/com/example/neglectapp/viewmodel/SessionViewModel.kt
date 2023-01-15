bd59f6bd81ca2e508efdeb7ce535c41cd3ff1d7f
Quintin <zelotbe@gmail.com>
2023-01-15 19:49:06 +0100

    Fixed bug session not adding to Room database

--- app/src/main/java/com/example/neglectapp/viewmodel/SessionViewModel.kt
+++ app/src/main/java/com/example/neglectapp/viewmodel/SessionViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.neglectapp.domain.model.HeftosSession
 import com.example.neglectapp.domain.repository.SessionRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import javax.inject.Inject
@@ -23,7 +24,7 @@ class SessionViewModel @Inject constructor(
     var amountInteracted = repo.getAmountInteractedFromRoom()
     var amountNotInteracted = repo.getAmountNotInteractedFromRoom()
 
-    fun addSession(session: HeftosSession) = viewModelScope.launch {
+    fun addSession(session: HeftosSession) = viewModelScope.launch(Dispatchers.IO) {
         repo.addSessionToRoom(session)
     }
 }
